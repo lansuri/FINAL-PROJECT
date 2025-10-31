c3793fd5e9ee24c7cef0c5d075b91fbcf765bf82
lansuri <lansurisuresh1612@gmail.com>
2025-10-31 12:15:17 +0530

    url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server:{
      host:"0.0.0.0",
      port: 8080,
-     allowedHosts:["fontend-brut.onrender.com"]
+     allowedHosts:["frontend-8sdy.onrender.com"]
   }
 })
